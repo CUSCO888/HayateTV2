893241596ef05db3130038feb1fa8dd9d7af3682
CUSCO888 <128247634+CUSCO888@users.noreply.github.com>
2026-03-11 03:48:36 +0900

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.hayatetv.app',
+  appName: 'HayateTV',
+  webDir: 'dist'
+};
+
+export default config;
